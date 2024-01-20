aea15498ac1963d307d7de5892e40f3fb5ea75f2
Victor Gomez <crivote@gmail.com>
2024-01-20 21:11:19 +0100

    more html

--- components/videos_video.js
+++ components/videos_video.js
@@ -33,20 +33,22 @@ export class Video extends Component {
 
     generatehtml() {
         console.log(this.data);
-        return `<div id="video${this.data.id}" class="videolist w-full bg-white border-b-2 border-slate200 rounded-md px-6 py-2 flex items-center gap-2">
-            <div class="mythumbvideo min-w-72 min-h-48 bg-cover bg-center bg-[url('${this.data.thumb_url}')]">
+        return `<div id="video${this.data.id}" class="videolist w-full bg-white border-b-2 border-slate200 rounded-md flex items-center">
+            <div class="mythumbvideo w-auto h-full min-w-72 min-h-48 bg-cover bg-center bg-[url('${this.data.thumb_url}')]">
                 <div class="hidden w-full">
-                ${Utils.videoembed(this.url)}
+                ${Utils.videoembed(this.data.url)}
                 </div>
             </div>
-            <diV
-                <h2 class="title text-xl font-semibold">${this.data.Title}</h2>
-                <p class="otherdata text-slate-600 font-semibold">${this.data.type}  ${this.data.Performer}</p>
-            </div>
-            <div class="flex gap-1 ml-auto items-center">
-                <span class="numrehearsal bg-slate-100 text-white p-2 rounded-lg">${this.data?.tunes ? `<i class="fa fa-circle-check text-green-600"></i>` : `<i class="fa fa-times-circle text-red-600"></i>`}</span>
-                <button class="editbutton bg-blue-400 p-1 rounded-md text-white text-bold" title="editar"><i class="fa fa-edit fa-fw fa-lg"></i></button>
-                <button class="deletebutton bg-red-400 p-1 rounded-md text-white text-bold" title="eliminar"><i class="fa fa-trash fa-fw fa-lg"></i></button>
+            <div class="p-6 flex">
+                <div>
+                    <h2 class="title text-xl font-semibold">${this.data.Title}</h2>
+                    <p class="otherdata text-slate-600 font-semibold">${this.data.type}  ${this.data.Performer}</p>
+                </div>
+                <div class="flex gap-1 ml-auto items-center">
+                    <span class="numrehearsal bg-slate-100 text-white p-2 rounded-lg">${this.data?.tunes ? `<i class="fa fa-circle-check text-green-600"></i>` : `<i class="fa fa-times-circle text-red-600"></i>`}</span>
+                    <button class="editbutton bg-blue-400 p-1 rounded-md text-white text-bold" title="editar"><i class="fa fa-edit fa-fw fa-lg"></i></button>
+                    <button class="deletebutton bg-red-400 p-1 rounded-md text-white text-bold" title="eliminar"><i class="fa fa-trash fa-fw fa-lg"></i></button>
+                </div>
             </div>
         </div>`;
     }
