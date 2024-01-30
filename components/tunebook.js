0d7bc4c66433d80a22a5cda90f85f4d2cda5f47f
Victor Gomez <crivote@gmail.com>
2024-01-30 22:49:05 +0100

    eslint

--- components/tunebook.js
+++ components/tunebook.js
@@ -3,43 +3,39 @@ import {Data} from '../startup.js';
 import {Tunesearch} from './tunebook_search.js';
 import {Tune} from './tunebook_tune.js';
 
+/**
+ * Componente repertorio personal
+ */
 export class Tunebook extends Component {
+  // filtro para temas del repertorio
   filtered = [];
-
   // instancias en DOM de las card tunes
   tune_instances = [];
-
+  // listas para filtros select
   typeslist = [];
   statuslist = [];
   toneslist = [];
+  // elemento html para los temas
   contentzone = null;
+  // formato por defecto
   format = 'card';
+  // array de subcomponentes instanciados
   subelements = [];
 
+  /**
+   * Constructor
+   *
+   * @param {string} name
+   * @param {HTMLBodyElement} parentel
+   */
   constructor(name, parentel) {
     super(name, parentel);
     this.setup();
   }
 
-  addListeners() {
-    this.element.querySelector('#tunebook_filter')
-        .addEventListener('input', this.applyFilter.bind(this));
-    this.element.querySelector('#typetune_filter')
-        .addEventListener('change', this.applyFilter.bind(this));
-    this.element.querySelector('#tonetune_filter')
-        .addEventListener('change', this.applyFilter.bind(this));
-    this.element.querySelector('#statustune_filter')
-        .addEventListener('change', this.applyFilter.bind(this));
-    this.element.querySelector('.addnewtune')
-        .addEventListener('click', this.launchsearch.bind(this));
-    this.element.querySelector('.resetfilter')
-        .addEventListener('click', this.resetFilter.bind(this));
-    this.element.querySelectorAll('.viewselector')
-        .forEach(
-            (el) => el.addEventListener('click', this.changeview.bind(this)),
-        );
-  }
-
+  /**
+   * Generar html del componente y asignar eventos
+   */
   async setup() {
     const typeslist = Data.tunebook.map((tune) => tune.tuneref.Type);
     this.typeslist = [...new Set(typeslist)];
@@ -49,57 +45,123 @@ export class Tunebook extends Component {
     this.tonelist = [...new Set(tonelist)];
     this.filtered = Data.tunebook;
 
+    const mycontent = this.generatehtml();
     // generate HTML
+    if (this.element) {
+      this.element.outerHTML = mycontent;
+    } else {
+      this.attachAt(mycontent, false);
+    }
     this.attachAt(this.generatehtml(), false);
     this.contentzone = this.element.querySelector('main');
     this.addListeners();
     this.rendertunes();
   }
 
+  /**
+   * add listeners to html elements
+   */
+  addListeners() {
+    // filtrar titulo
+    this.element.querySelector('#tunebook_filter')
+        .addEventListener('input', this.applyFilter.bind(this));
+    // fitrar tipo tema
+    this.element.querySelector('#typetune_filter')
+        .addEventListener('change', this.applyFilter.bind(this));
+    // filtrar por tono
+    this.element.querySelector('#tonetune_filter')
+        .addEventListener('change', this.applyFilter.bind(this));
+    // filtrar por status
+    this.element.querySelector('#statustune_filter')
+        .addEventListener('change', this.applyFilter.bind(this));
+    // quitar filtros
+    this.element.querySelector('.resetfilter')
+        .addEventListener('click', this.resetFilter.bind(this));
+    // añadir tema
+    this.element.querySelector('.addnewtune')
+        .addEventListener('click', this.launchsearch.bind(this));
+    // tipo de vista temas
+    this.element.querySelectorAll('.viewselector')
+        .forEach(
+            (el) => el.addEventListener('click', this.changeview.bind(this)),
+        );
+  }
+
+  /**
+   * Renderiza las instancias de tune y las guarda en this.instances
+   *
+   * @param {tunes[]} list
+   */
   rendertunes(list = Data.tunebook) {
     this.contentzone.innerHTML = '';
-    this.element.querySelector('.num_of_tunes').innerHTML = list.length + ' temas';
+    this.element.querySelector('.num_of_tunes').innerHTML =
+      list.length + ' temas';
     this.tune_instances = list.map((item) => {
       return new Tune('tune' + item.id, this.contentzone, item.id, this.format);
     });
   }
 
+  /**
+   * genera html general del componente
+   *
+   * @return {string}
+   */
   generatehtml() {
     return `<section id="${this.name}">
         <header class="p-6">
             <div class="flex flex-wrap items-center gap-2">
                 <h3 class="text-3xl">Mi repertorio</h3>
-                <span class="num_of_tunes bg-slate-400 text-sm px-2 py-1 uppercase text-slate-200 rounded-lg text-md">
+                <span class="num_of_tunes bg-slate-400 text-sm px-2 py-1 
+                uppercase text-slate-200 rounded-lg text-md">
                 ${Data.tunebook.length} temas</span></h3>
                 <span class="addnewtune text-blue-600 hover:text-blue-400">
                 <i class="fa fa-plus-circle fa-2x"></i></span>
                 <div class="ml-auto flex items-center gap-3">
-                    <span class="viewselector selected bg-slate-500 text-white" data-format="card"><i class="fa fa-grip fa-lg"></i></span>
-                    <span class="viewselector" data-format="list"><i class="fa fa-list fa-lg fa-fw"></i></span>
+                    <span class="viewselector selected bg-slate-500 
+                    text-white" data-format="card">
+                    <i class="fa fa-grip fa-lg"></i></span>
+                    <span class="viewselector" data-format="list">
+                    <i class="fa fa-list fa-lg fa-fw"></i></span>
                 </div>
-                <select id="typetune_filter"><option value="">Tipo</option><option> ${this.typeslist.join('</option><option>')}</option></select>
-                <select id="statustune_filter"><option value="">Status</option><option> ${this.statuslist.join('</option><option>')}</option></select>
-                <select id="tonetune_filter"><option value="">Tone</option><option> ${this.tonelist.join('</option><option>')}</option></select>
+                <select id="typetune_filter"><option value="">Tipo</option>
+                <option> ${this.typeslist.join('</option><option>')}
+                </option></select>
+                <select id="statustune_filter"><option value="">Status</option>
+                <option> ${this.statuslist.join('</option><option>')}
+                </option></select>
+                <select id="tonetune_filter"><option value="">Tone</option>
+                <option> ${this.tonelist.join('</option><option>')}
+                </option></select>
 
                 Filtrar <input type="search" id="tunebook_filter">
                 <i class="resetfilter fa fa-trash"></i>
             </div>
         </header>
-        <main class="p-6 ${this.format == 'card' ? 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4' : ''}"></main>
+        <main class="p-6 ${this.format == 'card' ?
+        `grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 
+        lg:grid-cols-4 xl:grid-cols-5 gap-4` : ''}"></main>
         </section>`;
   }
 
+  /**
+   * Vaciar filtros y pintar todos los temas
+   *
+   */
   resetFilter() {
-    this.filtered = Data.tunebook;
     this.element.querySelector('#tunebook_filter').value = '';
     this.element.querySelector('#typetune_filter').value = '';
     this.element.querySelector('#statustune_filter').value = '';
     this.element.querySelector('#tonetune_filter').value = '';
+    this.filtered = Data.tunebook;
     this.rendertunes();
   }
 
+  /**
+   * aplicar filtros en tiempo real y pintar temas filtrados
+   */
   applyFilter() {
-    const valstring = this.element.querySelector('#tunebook_filter').value.toLowerCase();
+    const valstring = this.element.querySelector('#tunebook_filter')
+        .value.toLowerCase();
     const valseltype = this.element.querySelector('#typetune_filter').value;
     const valselstat = this.element.querySelector('#statustune_filter').value;
     const valselton = this.element.querySelector('#tonetune_filter').value;
@@ -108,7 +170,9 @@ export class Tunebook extends Component {
           let val1 = true;
           if (valstring != '') {
             val1 = tune.Prefered_name.toLowerCase().includes(valstring) ||
-                    tune.tuneref.other_names.join(',').toLowerCase().includes(valstring);
+                tune.tuneref.other_names.join(',')
+                    .toLowerCase()
+                    .includes(valstring);
           }
           let val2 = true;
           if (valseltype != '') {
@@ -128,18 +192,27 @@ export class Tunebook extends Component {
     this.rendertunes(this.filtered);
   }
 
+  /**
+   * Cambiar formato presentacion temas y volver a renderizar
+   *
+   * @param {event} event
+   */
   changeview(event) {
     const myinput = event.currentTarget;
     const newformat = myinput.dataset.format;
     if (newformat != this.format) {
-      this.element.querySelector('.viewselector.selected').classList.remove('selected', 'bg-slate-500', 'text-white');
+      this.element.querySelector('.viewselector.selected')
+          .classList.remove('selected', 'bg-slate-500', 'text-white');
       myinput.classList.add('selected', 'bg-slate-500', 'text-white');
       this.format = myinput.dataset.format;
-      this.rendertunes();
       this.contentzone.classList.toggle('grid');
+      this.rendertunes();
     }
   }
 
+  /**
+   * Instanciar componente de búsqueda para añadir nuevo tema
+   */
   launchsearch() {
     this.subelements.push(new Tunesearch('tunesearch', this.element));
   }
--- components/tunebook_search.js
+++ components/tunebook_search.js
@@ -1,88 +1,146 @@
-import { Component } from "../abstract.js";
-import { Data } from "../startup.js";
-import { Tunesearchresult } from "./tunebook_search_result.js"; 
+import {Component} from '../abstract.js';
+import {Data} from '../startup.js';
+import {Tunesearchresult} from './tunebook_search_result.js';
 
+/**
+ * componente buscar temas para añadir a repertorio
+ */
 export class Tunesearch extends Component {
+  // todos los temas
+  tunes = Data.tunes;
+  // ids ya en repertorio
+  listoftunebookids;
+  // html element para resultados
+  resultszone;
+  // instancias de resultados
+  resultInstances;
 
-    tunes = Data.tunes;
-    listoftunebookids;
-    resultszone;
-    results;
+  /**
+   * Constructor
+   *
+   * @param {string} name
+   * @param {HTMLBodyElement} parentel
+   */
+  constructor(name, parentel) {
+    super(name, parentel);
+    this.setup();
+  }
 
-    constructor(name, parentel) {
-        super(name, parentel);
-        this.setup();
-    }
+  /**
+   * Generar html y añadir listeners
+   */
+  setup() {
+    // Create a list to exclude tunes already in the tunebook
+    this.listoftunebookids = Data.tunebook.map((tune) => tune.tunes_id);
 
-    setup() {
-        this.attachAt(this.generatehtml(), false);
-        this.resultszone = this.element.querySelector('.results');
-        this.addListeners();
+    this.attachAt(this.generatehtml(), false);
+    this.resultszone = this.element.querySelector('.results');
+    this.addListeners();
+  }
 
-        // Create a list to exclude tunes already in the tunebook
-        this.listoftunebookids = Data.tunebook.map(tune => tune.tunes_id);
-    }
-
-    generatehtml() {
-        return `<div id="${this.name}" class="animate__animated animate__slideInLeft fixed inset-0 w-1/3 bg-gray-400 flex p-6 shadow-lg">
+  /**
+   * Generar html elemento
+   *
+   * @return {string}
+   */
+  generatehtml() {
+    return `<div id="${this.name}" class="animate__animated animate__slideInLeft
+     fixed inset-0 w-1/3 bg-gray-400 flex p-6 shadow-lg">
             <div class="bg-white p-6 w-full rounded max-h-lvh overflow-auto">
-                <p id="closetunesearch" class="text-right" title="close"><i class="fa fa-times-circle fa-2x"></i></p>
-                <h2 class="text-2xl text-gray-400 font-bold mb-4">Añadir nuevos temas a tu repertorio</h2>
-                <input id="tunesearch" size=40" type="text" placeholder="escribe parte del nombre para ver resultados">
+                <p id="closetunesearch" class="text-right" title="close">
+                <i class="fa fa-times-circle fa-2x"></i></p>
+                <h2 class="text-2xl text-gray-400 font-bold mb-4">
+                Añadir nuevos temas a tu repertorio</h2>
+                <input id="tunesearch" size=40" type="text" 
+                placeholder="escribe parte del nombre para ver resultados">
                 <p class="info mt-6 mb-2"></p>
                 <div class="sugestion hidden">
-                    <input type="text" class="titlesugestion" size="40" placeholder="escriba el título del tune que desea">
-                    <button class="sendsugestion bg-blue-600 text-white p-2 rounded-md uppercase">enviar</button>
+                    <input type="text" class="titlesugestion" size="40" 
+                    placeholder="escriba el título del tune que desea">
+                    <button class="sendsugestion bg-blue-600 text-white p-2 
+                    rounded-md uppercase">enviar</button>
                 </div>
                 <ul class="results bg-slate-500 text-slate-50 p-2"></ul>
           </div>
         </div>`;
+  }
+
+  /**
+  * Añadir listeners al html
+  */
+  addListeners() {
+    // filtrar resultados
+    this.element.querySelector('#tunesearch')
+        .addEventListener('input', this.search.bind(this));
+    // cerrar componente
+    this.element.querySelector('#closetunesearch')
+        .addEventListener('click', this.eliminate.bind(this));
+    // enviar sugerencia
+    this.element.querySelector('.sendsugestion')
+        .addEventListener('click', this.suggestion.bind(this));
+  }
+
+  /**
+   * Buscar en todos los temas de la BD
+   *
+   * @param {event} event
+   */
+  search(event) {
+    const myinfo = this.element.querySelector('.info');
+    const suggestion = this.element.querySelector('.sugestion');
+    myinfo.textContent = '';
+    const string = event.target.value;
+
+    if (string.length > 0) {
+      // filtrar tunes excluyendo ya en el repertorio
+      const result = this.tunes.filter((tune) => {
+        return tune.other_names.some(
+            (name) => name.toLowerCase().includes(string.toLowerCase()),
+        ) && !this.listoftunebookids.includes(tune.id);
+      });
+
+      if (result.length > 0) {
+        suggestion.classList.add('hidden');
+        myinfo.textContent = `Encontrados ${result.length} resultados:`;
+        this.generateresults(result);
+      } else {
+        myinfo.textContent = `Sin resultados en la base de datos.
+            Si crees que el tema debería aparecer, por favor escribe el título
+            a continuación e intentaremos añadirlo. Gracias!`;
+        suggestion.classList.remove('hidden');
+      }
     }
+  }
 
-    addListeners() {
-        this.element.querySelector('#tunesearch').addEventListener('input', this.search.bind(this));
-        this.element.querySelector('#closetunesearch').addEventListener('click', this.eliminate.bind(this));
-    }
+  /**
+   * Generar instancias de resultados
+   *
+   * @param {array} items
+   */
+  generateresults(items) {
+    this.resultszone.innerHTML = '';
+    this.resultInstances = items.map((item) => {
+      return new Tunesearchresult('tune' + item.id, this.resultszone, item.id);
+    });
+  }
 
-    search(event) {
-        const mylist = this.element.querySelector('.results');
-        const myinfo = this.element.querySelector('.info');
-        mylist.innerHTML = '';
-        myinfo.textContent = '';
-        const string = event.target.value;
-        if (string.length > 0) {
-            let result = this.tunes.filter((tune) => {
-                return tune.other_names.some(
-                    name => name.toLowerCase().includes(string.toLowerCase())
-                ) && !this.listoftunebookids.includes(tune.id)
-            });
-            if (result.length > 0) {
-                this.element.querySelector('.sugestion').classList.add('hidden');
-                myinfo.textContent = `Encontrados ${result.length} resultados:`;
-                this.generateresults(result);
-            } else {
-                myinfo.textContent = `Sin resultados en la base de datos.
-                Si crees que el tema debería aparecer, por favor escribe el título
-                a continuación e intentaremos añadirlo. Gracias!`;
-                this.element.querySelector('.sugestion').classList.remove('hidden');
-            }
-        }
-    }
+  /**
+   * Ocultar el componente
+   */
+  eliminate() {
+    this.element.classList.remove('animate__slideInLeft');
+    this.element.classList.add('animate__slideOutLeft');
+    this.element.addEventListener('animationend', () => {
+      this.remove();
+    });
+  }
 
-    generateresults(items) {
-        this.resultszone.innerHTML = '';
-        this.results = items.map((item) => {
-            return new Tunesearchresult('tune' + item.id, this.resultszone, item.id);
-        });
+  /**
+   * Guarda sugerencias del usuario para añadir temas
+   *
+   * @param {event} event
+   */
+  async suggestion(event) {
 
-    }
-
-    eliminate() {
-        this.element.classList.remove('animate__slideInLeft');
-        this.element.classList.add('animate__slideOutLeft');
-        this.element.addEventListener('animationend', () => {
-            this.remove();
-        });
-    }
-
+  }
 }
